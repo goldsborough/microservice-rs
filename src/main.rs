21e29d74153b14cf6a13ee020cbfa3d2b5d401b6
Ivan Tham <pickfire@riseup.net>
2018-03-11 00:28:45 +0800

    Remove {} from Microservice

--- src/main.rs
+++ src/main.rs
@@ -274,7 +274,7 @@ fn main() {
     env_logger::init();
     let address = "127.0.0.1:8080".parse().unwrap();
     let server = hyper::server::Http::new()
-        .bind(&address, move || Ok(Microservice {}))
+        .bind(&address, move || Ok(Microservice))
         .unwrap();
     info!("Running microservice at {}", address);
     server.run().unwrap();
